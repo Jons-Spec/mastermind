1efee2f699848dd3c636aa389a3b807c39fb54a8
Jonatan Segal <jons@specialisterne.com>
2024-02-12 13:50:59 +0100

    Cleaned up prepackaged react stuff, set up folder structure and simple home page with routing

---
+++ frontend/src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import { Container } from "../../components/container/container";
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <h1>Mastermindo</h1>
+        <Button>Start</Button>
+      </Container>
+    </>
+  );
+}
